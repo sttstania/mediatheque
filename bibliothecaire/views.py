14a2fb076b9cea49924417958af3372a372c7259
sttstania <sttstania@users.noreply.github.com>
2025-10-13 14:17:16 +0200

    baseHTML, liste media, correction models, views

--- bibliothecaire/views.py
+++ bibliothecaire/views.py
@@ -3,30 +3,32 @@ from django.shortcuts import render, redirect
 from bibliothecaire.models import CreationMembre, Membre, Livre, JeuDePlateau, CD, DVD
 
 
+
+
 def creation_membre(request):
     if request.method == "POST":
         form = CreationMembre(request.POST)
         if form.is_valid():
             nom = form.cleaned_data['nom']
             Membre.objects.create(nom=nom)
-            return redirect("liste_membre")
+            return redirect("liste_membres")
     else:
         form = CreationMembre()
     return render(request, "creation_membre.html", {"form": form})
 
 
 
-def liste_membre(request):
+def liste_membres(request):
     membre = Membre.objects.all()
-    return render(request,'liste_membre.html',{'membre':membre})
+    return render(request,'liste_membres.html',{'membres':membre})
 
 def liste_media(request):
-    livre = Livre.objects.all()
+    livres = Livre.objects.all()
     cds = CD.objects.all()
     dvds = DVD.objects.all()
     jeux = JeuDePlateau.objects.all()
     return render(
         request,
         'liste_media.html',
-        {'livres': livre, 'cds': cds,'dvds': dvds,'jeux': jeux},
+        {'livres': livres, 'cds': cds,'dvds': dvds,'jeux': jeux},
     )
